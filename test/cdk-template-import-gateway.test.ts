dbe1025da3c14aa635b47a7ceb35eefa4ae9a278
joaobispo2077 <joaobispo2077@gmail.com>
2025-11-03 14:18:34 -0300

    chore: setup CDK infra

---
+++ test/cdk-template-import-gateway.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkTemplateImportGateway from '../lib/cdk-template-import-gateway-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-template-import-gateway-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkTemplateImportGateway.CdkTemplateImportGatewayStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

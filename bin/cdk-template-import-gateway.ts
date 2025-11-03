790c6c608ee58447fb7bfd2a853a9ad8dffce00a
joaobispo2077 <joaobispo2077@gmail.com>
2025-11-03 18:29:05 -0300

    feat: init constructor

--- bin/cdk-template-import-gateway.ts
+++ bin/cdk-template-import-gateway.ts
@@ -1,20 +1,70 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib/core';
 import { CdkTemplateImportGatewayStack } from '../lib/cdk-template-import-gateway-stack';
+import {
+	BaseConfig,
+	ExtraTags,
+	getEnvironmentConfig,
+} from '../configs/cdk/config';
+import {
+	createSynthesizer,
+	SynthesizerSSMConfig,
+} from './synthesizer/create-synthesizer';
 
-const app = new cdk.App();
-new CdkTemplateImportGatewayStack(app, 'CdkTemplateImportGatewayStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+async function main() {
+	const app = new cdk.App();
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+	const stage = app.node.tryGetContext('stage') ?? 'dev';
+	const envName =
+		(app.node.tryGetContext('env') as 'dev' | 'uat' | 'prod') ?? 'dev';
+	const region = app.node.tryGetContext('region') ?? 'us-east-1';
+	console.log(
+		`▶️  Deploying stage: ${stage}, env: ${envName}, region: ${region}`,
+	);
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+	const configs = getEnvironmentConfig(stage);
+	console.log('Config:', {
+		...configs,
+	});
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+	const defaultExtraTags: ExtraTags = {
+		ServiceName: configs.SERVICE_NAME,
+		EnvironmentName: configs.ENV_NAME,
+		DeployedBy: 'CDK',
+		Region: configs.REGION,
+		Stage: stage,
+	};
+
+	const synthesizerSSMConfig: SynthesizerSSMConfig = {
+		cdkBootstrapConfigSSM: configs.CDK_BOOTSTRAP_CONFIG_SSM,
+		bucketPrefix: configs.SERVICE_NAME,
+		region: configs.REGION,
+	};
+	console.debug(
+		'SynthesizerSSMConfig:',
+		JSON.stringify(synthesizerSSMConfig, null, 2),
+	);
+
+	const synthesizer = await createSynthesizer(synthesizerSSMConfig);
+	console.debug('Synthesizer instance:', synthesizer);
+
+	const baseStackProps: BaseConfig = {
+		serviceName: configs.SERVICE_NAME,
+		params: configs,
+		env: { region: configs.REGION, stage: stage },
+	};
+
+	new CdkTemplateImportGatewayStack(app, 'CdkTemplateImportGatewayStack', {
+		stackName: `CdkTemplateImportGatewayStack-${stage}`,
+		description: `CDK Template Import Gateway Stack - ${stage} environment`,
+		env: { region: configs.REGION },
+		base: baseStackProps,
+		extraTags: defaultExtraTags,
+		synthesizer,
+	});
+}
+
+main().catch((e) => {
+	console.error(e);
+	process.exit(1);
 });
